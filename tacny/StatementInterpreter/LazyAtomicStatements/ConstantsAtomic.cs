d4327bfe2fc39b25d3e1fa81201ab32a2a1b96fc
Vytautas <vtumas@gmail.com>
2016-05-19 09:27:51 +0100

    split atomic

---
+++ tacny/StatementInterpreter/LazyAtomicStatements/ConstantsAtomic.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Dafny;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Tacny;
+using System.Linq;
+
+namespace LazyTacny {
+
+  public class ConstantsAtomic : Atomic, IAtomicLazyStmt {
+
+
+    public ConstantsAtomic(Atomic atomic) : base(atomic) { }
+
+    public IEnumerable<Solution> Resolve(Statement st, Solution solution) {
+      throw new NotImplementedException();
+    }
+
+
+    private IEnumerable<Solution> Constants(Statement st) {
+      IVariable lv = null;
+      List<Expression> callArgs;
+      var result = new List<Expression>();
+      InitArgs(st, out lv, out callArgs);
+      Contract.Assert(lv != null, Util.Error.MkErr(st, 8));
+      Contract.Assert(callArgs.Count == 1, Util.Error.MkErr(st, 0, 1, callArgs.Count));
+
+      foreach(var arg1 in ResolveExpression(callArgs[0])) {
+        var expression = arg1 as Expression;
+        Contract.Assert(expression != null, Util.Error.MkErr(st, 1, "Expression"));
+        var expt = ExpressionTree.ExpressionToTree(expression);
+        var leafs = expt.GetLeafData();
+        foreach(var leaf in leafs) {
+          if(leaf is LiteralExpr) {
+            if(!result.Exists(j => (j as LiteralExpr)?.Value == (leaf as LiteralExpr)?.Value)) {
+              result.Add(leaf);
+            }
+          } else if(leaf is ExprDotName) {
+            
+            var edn = leaf as ExprDotName;
+            var ns = edn.Lhs as NameSegment;
+            if (result.Exists(j => ExprDotEquality(j, edn))) {
+              result.Add(leaf);
+            }
+          }
+        }
+
+      }
+
+      yield return AddNewLocal(lv, result);
+    }
+
+
+    private static bool ExprDotEquality(Expression a, ExprDotName b) {
+      var dotName = a as ExprDotName;
+      if (dotName == null)
+        return false;
+      var nsA = dotName.Lhs as NameSegment;
+      var nsB = b.Lhs as NameSegment;
+      return nsA.Name == nsB.Name && dotName.AsStringLiteral() == b.AsStringLiteral();
+    }
+  }
+}
---
+++ tacny/StatementInterpreter/LazyAtomicStatements/SplitAtomic.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Dafny;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Tacny;
+namespace LazyTacny {
+
+  public class SplitAtomic : Atomic, IAtomicLazyStmt {
+
+
+    public SplitAtomic(Atomic atomic) : base(atomic) { }
+
+    public IEnumerable<Solution> Resolve(Statement st, Solution solution) {
+      return Split(st);
+    }
+
+    private IEnumerable<Solution> Split(Statement st) {
+      IVariable lv = null;
+      List<Expression> callArgs;
+      List<Expression> result = new List<Expression>();
+      InitArgs(st, out lv, out callArgs);
+      Contract.Assert(lv != null, Util.Error.MkErr(st, 8));
+      Contract.Assert(callArgs.Count == 2, Util.Error.MkErr(st, 0, 2, callArgs.Count));
+      foreach (var arg1 in ResolveExpression(callArgs[0])) {
+        var expression = arg1 as BinaryExpr;
+        if (expression == null)
+          break;
+        foreach (var arg2 in ResolveExpression(callArgs[1])) {
+          var litVal = arg2 as LiteralExpr;
+          Contract.Assert(litVal != null);
+          var op = StringToOp(litVal.Value.ToString());
+          result = SplitExpression(op, expression);
+          yield return AddNewLocal(lv, result);
+        }
+      }
+    }
+
+    private List<Expression> SplitExpression(BinaryExpr.Opcode op, BinaryExpr expression) {
+      var expList = new List<Expression>();
+
+      if (!expression.Op.Equals(op)) {
+        expList.Add(expression);
+      } else if (IsChained(expression) && op.Equals(BinaryExpr.Opcode.And)) {
+        expList.Add(expression);
+      } else {
+        if (!(expression.E0 is BinaryExpr))
+          expList.Add(expression.E0);
+        else {
+          expList.AddRange(SplitExpression(op, expression.E0 as BinaryExpr));
+        }
+        if (!(expression.E1 is BinaryExpr))
+          expList.Add(expression.E1);
+        else {
+          expList.AddRange(SplitExpression(op, expression.E1 as BinaryExpr));
+        }
+      }
+      return expList;
+    }
+
+
+    private bool IsChained(BinaryExpr expression) {
+      if (!expression.Op.Equals(BinaryExpr.Opcode.And))
+        return false;
+      var lhs = expression.E0 as BinaryExpr;
+      var rhs = expression.E1 as BinaryExpr;
+      if (lhs == null || rhs == null)
+        return false;
+
+      return BinaryExpr.IsEqualityOp(lhs.Op) && BinaryExpr.IsEqualityOp(rhs.Op);
+
+    }
+  }
+}
