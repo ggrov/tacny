3d3717fa7106a303c10e3e76b6f06d0cf9287cc4
Yuhui Lin <lyhlbsl@gmail.com>
2016-08-31 10:39:13 +0100

    projecttion: lemmas

---
+++ new-dafny/Source/Dafny/Tacny/Function/Lemmas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics.Contracts;
+using Microsoft.Dafny;
+
+namespace Tacny.Function{
+  class Lemmas : Projection{
+    public override string Signature => "lemmas";
+    public override int ArgsCount => 0;
+
+    private bool isLemma(MemberDecl m){
+      return (m is Lemma || m is FixpointLemma);
+    }
+    /// <summary>
+    /// Return the objects of all the lemms
+    /// </summary>
+    /// <param name="expression"></param>
+    /// <param name="proofState"></param>
+    /// <returns></returns>
+    public override IEnumerable<object> Generate(Expression expression, ProofState proofState){
+      var ls = proofState.Members.Values.ToList().Where(isLemma);
+      yield return ls;
+    }
+  }
+}
